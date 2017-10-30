9b2e893f4e3e76b50618c08d7d2d08ef451e8303
Unknown <danielarinez@hotmail.com>
2017-10-30 21:13:30 +0100

    WIP: Builgind a shortener-url. NPM init and project structure

---
+++ app.js
@@ -0,0 +1,22 @@
+// require and instantiate express
+var express = require('express');
+var app = express();
+// we'll need the path module to correctly concatenate our paths
+var path = require('path');
+
+module.exports.encode = encode;
+module.exports.decode = decode;
+module.exports = Url;
+
+
+// tell Express to serve files from our public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  // route to serve up the homepage (index.html)
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+var server = app.listen(3000, function(){
+  console.log('Server listening on port 3000');
+});
